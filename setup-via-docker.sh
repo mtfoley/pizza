7f2e34ed7ab7199297abb9cdcb6366c22c61f852
mtfoley <mtfoley.mae@gmail.com>
2023-08-31 08:28:09 -0400

    fetch repos using org, skip archived repos by default

--- setup-via-docker.sh
+++ setup-via-docker.sh
@@ -25,30 +25,31 @@ if [[ "$1" = "start" ]]; then
         -database postgres://postgres:pw@pizza-oven-db:5432?sslmode=disable \
         -verbose \
         up
-    docker run --rm \
-        --detach \
-        --name pizza-app \
-        --network pizza \
-        -v $(pwd):$(pwd) \
-        -w $(pwd) \
-        -p "8080:8080" \
-        golang:1.21-alpine \
-        go run main.go
+    # docker run --rm \
+    #     --detach \
+    #     --name pizza-app \
+    #     --network pizza \
+    #     -v $(pwd):$(pwd) \
+    #     -w $(pwd) \
+    #     -p "8080:8080" \
+    #     golang:1.21-alpine \
+    #     go run main.go
 
-    for i in `seq 1 10`;
-    do
-        echo Pinging Pizza Oven
-        sleep 5
-        curl -s --connect-timeout 1 http://localhost:8080/ping -o /dev/null && \
-            break
-    done
-    echo Posting to /bake endpoint with https://github.com/open-sauced/insights
-    curl -s -d '{"url":"https://github.com/open-sauced/insights"}' \
-        -H "Content-Type: application/json" \
-        http://localhost:8080/bake
+#    for i in `seq 1 10`;
+#    do
+#        echo Pinging Pizza Oven
+#        sleep 5
+#        curl -s --connect-timeout 1 http://localhost:8080/ping -o /dev/null && \
+#            break
+#    done
+#    echo Posting to /bake endpoint with https://github.com/open-sauced/insights
+#   curl -s -d '{"org":"https://github.com/open-sauced"}' \
+#       -H "Content-Type: application/json" \
+#       http://localhost:8080/bake
+    #docker logs pizza-app --follow
 fi
 if [[ "$1" = "stop" ]]; then
-    docker stop pizza-app
+    #docker stop pizza-app
     docker stop pizza-oven-db
     docker network rm pizza
 fi
